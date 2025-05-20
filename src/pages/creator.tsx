d00885b4ab432572a09ed33b97bd502eb0895cd5
Michael Kargl <michael.kargl@posteo.net>
2025-05-20 15:38:55 +0200

    feat: adds minimalistic scanner and creator

--- src/pages/creator.tsx
+++ src/pages/creator.tsx
@@ -1,10 +1,31 @@
-import React from "react";
+import React, {useEffect} from "react";
 import {ReactElement} from "react";
 import {Layout} from "../layout";
+import encodeQR from 'qr';
+import decodeQR from "qr/decode.js";
 
 const CreatorPage: React.FC = (): ReactElement => {
+    const [qrContent, setQrContent] = React.useState<string>("");
+    const [qrCode, setQrCode] = React.useState<string>("");
+
+    useEffect(() => {
+        const qrCode = encodeQR(qrContent, 'ascii');
+        setQrCode(qrCode);
+    }, [qrContent]);
+
     return (<div className='creator-page-component'>
         <Layout>
+            <fieldset>
+                <div className='field-row-stacked'>
+                    <label htmlFor='qr-content'>QR Content</label>
+                    <textarea id='qr-content' rows={8} onChange={(e) => setQrContent(e.target.value)}>
+                        {qrContent}
+                    </textarea>
+                    <pre>
+                        {qrCode}
+                    </pre>
+                </div>
+            </fieldset>
             Creator Page
         </Layout>
     </div>)
